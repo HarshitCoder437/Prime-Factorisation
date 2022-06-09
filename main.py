81ac9617c7a9aa2163aa17999ba6c5e4c8d74d87
krishna2803 <kpandey81930@gmail.com>
2022-06-09 18:00:50 +0530

    fixed code

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-num = 12
-prime = []
+num = int(input('n: '))
+factors = []
 
-for i in range(1,num):
-    for j in range(2, i): 
-        if i % j == 0:
-            prime.append(j)
-print(prime)
+for i in range(2, num):
+    while num % i == 0:
+        factors.append(i)
+        num //= i
+
+print(*factors, sep=' x ')
